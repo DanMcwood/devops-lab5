3e027ad52c343595efd353b6e562930cb373c428
DanMcwood <danila.medvedkovo21@gmail.com>
2026-04-03 09:10:04 +0000

    add CI/CD

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,24 +18,55 @@ users = [
     }
 ]
 
-def test_get_existed_user():
-    '''Получение существующего пользователя'''
-    response = client.get("/api/v1/user", params={'email': users[0]['email']})
-    assert response.status_code == 200
-    assert response.json() == users[0]
 
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'noone@mail.com'})
+    assert response.status_code == 404
+
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "Test User",
+        "email": "test_user@mail.com"
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code in [200, 201]
+
+    user_id = response.json()
+    assert isinstance(user_id,int)
+
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    new_user = {
+        "name": "Duplicate",
+        "email": users[0]["email"]
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code in [400, 409]
+
 
 def test_delete_user():
-    '''Удаление пользователя'''
-    pass
+    new_user = {
+        "name": "Delete Me",
+        "email": "delete_me@mail.com"
+    }
+
+    # создаем пользователя
+    create_response = client.post("/api/v1/user", json=new_user)
+    assert create_response.status_code in [200, 201]
+
+    # удаляем по email
+    delete_response = client.delete(
+        "/api/v1/user",
+        params={"email": new_user["email"]}
+    )
+
+    assert delete_response.status_code == 204
+
+    # проверяем что удалился
+    get_response = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert get_response.status_code == 404
